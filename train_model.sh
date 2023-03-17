95c9a217eef48355a3258339481a3b32426ea7ef
dan nelson <dan.nelson8@gmail.com>
2023-03-17 19:56:41 +0000

    working training code

--- train_model.sh
+++ train_model.sh
@@ -3,16 +3,16 @@
 torchrun --nproc_per_node=4 --master_port=9292 train.py \
     --model_name_or_path /src/weights/llama-7b \
     --tokenizer_name_or_path /src/weights/tokenizer \
-    --data_path ./alpaca_data.json \
+    --data_path ./simpsons_data_two.json \
     --bf16 True \
-    --output_dir alpaca_out \
-    --num_train_epochs 3 \
+    --output_dir homer_out \
+    --num_train_epochs 4 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 2000 \
+    --save_strategy "epoch" \
+    --save_total_limit 3 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
