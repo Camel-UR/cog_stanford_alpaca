e7ed0ea4051480664bf54e66ed7cdf883b26672a
dan nelson <dan.nelson8@gmail.com>
2023-03-17 20:05:09 +0000

    working prediction code

--- predict.py
+++ predict.py
@@ -3,27 +3,28 @@ from cog import BasePredictor, Input
 from transformers import LLaMAForCausalLM, LLaMATokenizer
 import torch
 
-from train import PROMPT_DICT
-PROMPT = PROMPT_DICT['prompt_no_input']
+from train import PROMPT
 
-CACHE_DIR = 'alpaca_out'
+CACHE_DIR = 'homer_out'
 
 class Predictor(BasePredictor):
     def setup(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = LLaMAForCausalLM.from_pretrained("alpaca_out", cache_dir=CACHE_DIR, local_files_only=True)
-        self.model = self.model
+        self.model = LLaMAForCausalLM.from_pretrained("final_homer_two_out", cache_dir=CACHE_DIR, local_files_only=True)
+        self.model = self.model.half()
         self.model.to(self.device)
-        self.tokenizer = LLaMATokenizer.from_pretrained("alpaca_out", cache_dir=CACHE_DIR, local_files_only=True)
+        self.tokenizer = LLaMATokenizer.from_pretrained("final_homer_two_out", cache_dir=CACHE_DIR, local_files_only=True)
+        self.tokenizer.pad_token_id = 0
 
     def predict(
         self,
-        prompt: str = Input(description=f"Prompt to send to LLaMA."),
+        prompt: str = Input(description=f"Prompt to sened to model.", default=PROMPT),
+        character: str = Input(description=f"Simpsons character to respond as", default="Homer Simpson"),
         n: int = Input(description="Number of output sequences to generate", default=1, ge=1, le=5),
         max_length: int = Input(
             description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
             ge=1,
-            default=50
+            default=500
         ),
         temperature: float = Input(
             description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.",
@@ -44,8 +45,10 @@ class Predictor(BasePredictor):
             default=1
         )
         ) -> List[str]:
-        format_prompt = PROMPT.format_map({'instruction': prompt})
-        input = self.tokenizer(format_prompt, return_tensors="pt").input_ids.to(self.device)
+        prompt = ''.join([prompt, '\n', character, ':'])
+        print(f'final prompt:\n{prompt}')
+        input = self.tokenizer(prompt, return_tensors="pt").input_ids.to(self.device)
+        temperature = float(temperature)
 
         outputs = self.model.generate(
             input,
@@ -58,6 +61,6 @@ class Predictor(BasePredictor):
         )
         out = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         # removing prompt b/c it's returned with every input 
-        out = [val.split('Response:')[1] for val in out]
+        out = [val.split(f'{character}:')[-1] for val in out]
         return out
         
